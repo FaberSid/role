2e2284a5dbc28cef3a0702c6526646b25bcead93
MMOKUN <46297678+MMOKUN@users.noreply.github.com>
2019-04-21 19:18:39 +0900

    Update MMOくん.py

--- MMOくん.py
+++ MMOくん.py
@@ -410,7 +410,7 @@ async def on_message(message):
                     page -= 1
                 elif react.emoji == right:
                     page += 1
-                await client.message(content=msg,embed=embed)
+                await client.edit_message(content=None,msg)
 
         i = 1
         member_data = []
