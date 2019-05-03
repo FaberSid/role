d87b02ef8b591ce9cbae490f2e661c039614ff29
MMOKUN <46297678+MMOKUN@users.noreply.github.com>
2019-05-03 13:03:04 +0900

    Update MMOくん.py

--- MMOくん.py
+++ MMOくん.py
@@ -252,7 +252,7 @@ async def on_message(message):
 
         i = 1
         member_data = ""
-        channel = role1 = discord.utils.get(message.server.roles,name=client.get_channel(f"{int(row[0])}").name[:-4]).mention
+        channel = discord.utils.get(message.server.roles,name=client.get_channel(f"{int(row[0])}").name[:-4]).mention
 
         for row in db_get_tsukishima():
             member_data += "{0}位:『{1}』[`合計:{2}体`]\n".format(i,channel,int(row[1]))
